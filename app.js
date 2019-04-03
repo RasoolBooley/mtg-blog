1ec3d92c5a6300f34b96c8eb3bbcf9ce4dfdcde8
RasoolBooley <rasool.booley@gmail.com>
2019-04-03 15:22:55 +0200

    added create js

--- app.js
+++ app.js
@@ -9,8 +9,17 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var createRouter = require('./routes/create');
-var createRouter = require('./routes/update');
-var createRouter = require('./routes/delete');
+var updateRouter = require('./routes/update');
+var deleteRouter = require('./routes/delete');
+
+var routes = [
+  require('./routes/index'),
+  require('./routes/users'),
+  require('./routes/create'),
+  require('./routes/view'),
+  require('./routes/update'),
+  require('./routes/delete')
+]
 
 var app = express();
 
@@ -18,14 +27,15 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -43,6 +53,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+// module.exports = app;
 
-app.listen(8080);
+app.listen(4000);
---
+++ routes/create.js
@@ -0,0 +1,72 @@
+
+
+var express = require('express');
+var router = express.Router();
+var Posts = require('../db.json');
+var request = require  ('request');
+
+// get create page
+
+router.get('/', function(req, res, next) {
+    res.render('create',{
+        title: "Create Page"
+    });
+});
+
+// post a create request
+
+router.post('/', function(req, res, next){
+    var posts = Posts.posts;
+
+    // Get the ID of the last post
+    var id = Posts[posts.length-1].id;
+
+    // get id for the last post
+    id = Number (id)+1;
+
+    // getting the date
+    var newDate = new Date();
+
+    // Choose Date format
+    var date = '${newDate.getDate()}/${newDate.getMonth()}/${newDate.getFullYear()}';
+
+    // get the content of the post
+    var content = req.body.content;
+
+    // text is used for the description
+    // Turns the object into a string
+    var text = JSON.stringify(content);
+
+    // description variable
+    var description;
+    description = text.charAt(1);
+
+    // this gets the character or char from the string 
+    for(var i=2; i<200; i++){
+        description += text.charAt(i);
+    }
+
+    // Post request
+    request({
+        url: 'localhost:3004',
+        method: Posts,
+        form: {
+            id: id,
+            date: date,
+            title: req.body.title,
+            image: req.body.image,
+            description: description + "...</p>",
+            content: content,
+            author: req.app.locals.user,
+        },
+
+        function(error,reponse,body){
+            res.render('index', {message:"Added!"});
+        }
+    })
+            // redirect to homepage after post is created
+            res.redirect("..");
+})
+
+            module.exports = router;
+        
